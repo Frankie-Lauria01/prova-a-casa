926cf494a697256a16f73e33662f2a4371d983c2
Francesco L <Francesco L@DESKTOP-POGLVGS.fritz.box>
2019-03-25 17:52:02 +0100

    seconda foto

--- src/prova_casa/Prova_casa.java
+++ src/prova_casa/Prova_casa.java
@@ -15,7 +15,7 @@ public class Prova_casa {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ciaoioaioioiaoioaioi
     }
     
 }
